138bdde45bd8b2aa5e826866868de3ab53717cbf
Pedro Soares <pedroneto.ps180@gmail.com>
2020-09-02 11:43:11 -0300

    personal concept

---
+++ script.js
@@ -0,0 +1,33 @@
+console.clear();
+// const modal = document.querySelector('.modal-logo-card');
+// const modalClose = document.querySelector('.modal--close');
+// const shadowContainer = document.querySelector('.shadow-container');
+
+console.log('test');
+
+window.addEventListener('click', () => {
+	console.log('window');
+});
+
+function showContent(type) {
+	console.log('test');
+	// modal.style.display = '';
+	// shadowContainer.style.display = '';
+	// const laptopLogo = document.querySelector('.' + type + '-logo');
+	// const contentLogo = document.querySelector('.modal-content-logo');
+	// let logoImg = document.createElement('img');
+	// logoImg.src = logo.src;
+	// contentLogo.appendChild(logoImg);
+}
+
+// modalClose.onclick = function() {
+//     modal.style.display = 'none';
+// 		shadowContainer.style.display = 'none';
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = 'none';
+// 				shadowContainer.style.display = 'none';
+//     }
+// }
